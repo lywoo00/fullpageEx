dc403af59d7aa1395bc0984e9ddaf6ef302ebbdf
lywoo00 <lywoo00@naver.com>
2021-05-12 10:34:45 +0900

    풀페이지 구현

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
 		autoScrolling:true,
-		scrollHorizontally: true
+		scrollHorizontally: true,
+        sectionsColor : ['lightgreen', 'skyblue', 'orange', 'tomato'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage'],
+	    menu: '#menu',
+
+        //Navigation
+        navigation: true,
+        navigationPosition: 'right',
 	});
 });
